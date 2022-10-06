3bc1d913c8543dfa14101d42562ee59f4b749a46
Kai-Nalu <kainietogonzalez@gmail.com>
2022-10-06 11:16:33 -0400

    Update makeLabelImage.js

--- makeLabelImage.js
+++ makeLabelImage.js
@@ -90,6 +90,7 @@ exports.makeLabelImage = function (ticketKey, ticketName, ticketReporter, ticket
         context.textAlign = 'right';
         context.fillText(birthdayLabelText, width-birthdayWidth, copiesBirthdayY+topMargin);
         
-        resolve(context.toBuffer('image/png'));
+        const buffer = context.toBuffer('image/png');
+        resolve(buffer);
     });
 };
